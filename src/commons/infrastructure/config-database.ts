590afbda98306d04a88987dc94be4143dfc301b7
Jonathan.Cardenas <greiff.jonathan@gmail.com>
2024-06-20 04:33:43 -0500

    chore: Update npm dependencies and add @nestjs/swagger for API documentation

--- src/commons/infrastructure/config-database.ts
+++ src/commons/infrastructure/config-database.ts
@@ -3,7 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 export const CONFIG_DATABASE = () =>
   TypeOrmModule.forRoot({
     type: 'postgres',
-    host: 'host.docker.internal',
+    host: 'localhost', //'host.docker.internal'
     port: 5432,
     username: 'postgres',
     password: 'postgres',
