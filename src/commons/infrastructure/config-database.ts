527b0a2f2ece181e7fe9b34241252a2c2fed6c96
Jonathan.Cardenas <greiff.jonathan@gmail.com>
2024-06-20 02:53:21 -0500

    chore: Update Dockerfile and package dependencies

--- src/commons/infrastructure/config-database.ts
+++ src/commons/infrastructure/config-database.ts
@@ -3,7 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 export const CONFIG_DATABASE = () =>
   TypeOrmModule.forRoot({
     type: 'postgres',
-    host: 'localhost',
+    host: 'host.docker.internal',
     port: 5432,
     username: 'postgres',
     password: 'postgres',
